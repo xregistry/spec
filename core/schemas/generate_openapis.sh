16ead01401b65f254c7d611eb233ca6318985477
Clemens Vasters <clemens@vasters.com>
2023-06-27 14:41:08 +0200

    script and model fixes
    Signed-off-by: Clemens Vasters <clemens@vasters.com>

--- core/schemas/generate_openapis.sh
+++ core/schemas/generate_openapis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-../core/schemas/generate_openapi.sh
-../schema/schemas/generate_openapi.sh
-../endpoint/schemas/generate_openapi.sh
-../message/schemas/generate_openapi.sh
+$(dirname "$0")/../../core/schemas/generate_openapi.sh
+$(dirname "$0")/../../schema/schemas/generate_openapi.sh
+$(dirname "$0")/../../endpoint/schemas/generate_openapi.sh
+$(dirname "$0")/../../message/schemas/generate_openapi.sh
